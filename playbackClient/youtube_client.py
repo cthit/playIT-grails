1fe00516b4fe928ee78b980c01c20b92eaa9b074
Johan Andersson <horv@chalmers.it>
2013-07-27 22:27:56 +0200

    Client script written in python added

---
+++ playbackClient/youtube_client.py
@@ -0,0 +1,85 @@
+
+import json
+import urllib.request
+import time
+import subprocess
+import os
+import argparse
+
+SERVER = ""
+SHOW_VIDEOS = "youTubeInTheHubbServer/video/popQueue"
+MONITOR_NUMBER = 1
+
+def main():
+	_init()
+	_mainLoop()
+
+
+def _init():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-mn', '--monitorNumber')
+	parser.add_argument('-s', '--server')
+	args = parser.parse_args()
+
+	if(args.server == None):
+		print("Please supply a server by: -s http://example.com")
+		exit()
+	else:
+		global SERVER
+		SERVER = _fixServerAdress(args.server)
+
+	if(args.monitorNumber != None):
+		global MONITOR_NUMBER
+		MONITOR_NUMBER = args.monitorNumber
+
+def _fixServerAdress(rawServer):
+		if(not rawServer.endswith("/")):
+			rawServer = rawServer + "/"
+		if(not rawServer.startswith("http://")):
+			rawServer = "http://"+rawServer
+		return rawServer
+
+def _mainLoop():
+
+	while(True):
+		youtubeID = _loadYTID()
+		if(youtubeID != None):
+			print("Playing video with id: "+youtubeID)
+			_playVideo(youtubeID)
+		else:
+			print("No item in queue, sleeping...")
+			time.sleep(10)
+
+
+def _loadYTID():
+	url = urllib.request.urlopen(SERVER+SHOW_VIDEOS)
+	raw_data = url.read().decode("utf8")
+
+	if(raw_data == "[]"):
+		return None
+	else:
+		data = json.loads(raw_data)
+		return data.get("youtubeID")
+
+def _playVideo(youtubeID):
+	print("_playVideo: "+youtubeID)
+	youtubeURL = "'http://www.youtube.com/watch?v="+youtubeID+"'"
+	command = 'mplayer -cache 4096 -fs -xineramascreen '+str(MONITOR_NUMBER)+' "$(youtube-dl -g '+youtubeURL+')"'
+	print(command)
+	os.system(command)
+	#subprocess.Popen(command)
+	#subprocess.call(["mplayer",'"$(youtube-dl -g '+youtubeURL+')"'])
+	#"-cache 4096","-fs","-xineramascreen 1",
+if __name__=="__main__":
+	main()
+
+
+
+
+
+
+
+
+
+
+
